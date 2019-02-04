83cb272d011133a6b5f0788d7d8e1996df797be1
krytoes <46593301+krytoes@users.noreply.github.com>
2019-02-04 11:20:43 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -2,6 +2,6 @@ num1 = float(input("Input a number: "))
 if num1 > 0:
    print("positive")
 elif num1 == 0:
-   print("It is Zero")
+   print("Zero")
 else:
-   print("negative ")
+   print("negative")
