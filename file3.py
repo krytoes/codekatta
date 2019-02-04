d00f496fb89ad2b956327061d263d7d5d37e6fcf
krytoes <46593301+krytoes@users.noreply.github.com>
2019-02-04 14:39:03 +0530

    Update file3.py

--- file3.py
+++ file3.py
@@ -3,4 +3,4 @@ if(str=='A' or str=='a' or str=='E' or str =='e' or str=='I'
  or str=='i' or str=='O' or str=='o' or str=='U' or str=='u'):
     print(str, "Vowel")
 else:
-    print(str, "Consonant")
+    print("Consonant")
